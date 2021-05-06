1ae06da9778ba105e6a217c48a04c096d2e46a0c
victoraugustosouza <victor_engenharia@poli.ufrj.br>
2021-05-06 19:15:20 -0300

    + comments to all functions, changed @close_conn to @fim

--- tarefa 4/cli.py
+++ tarefa 4/cli.py
@@ -1,6 +1,6 @@
 #Ver documentação em: https://rpyc.readthedocs.io/en/latest/
 
-# Cliente de echo usando RPC
+
 import rpyc #modulo que oferece suporte a abstracao de RPC
 
 # endereco do servidor de echo
@@ -16,33 +16,43 @@ def iniciaConexao():
 
 	return conn
 def printt(texto):
+	#Função de callback assíncrono. Será chamada quando houver retorno do servidor.
 	print(texto)
 
 def login(conn):
-    msg=input("entre com o seu nome de usuário, sem espaços: ")
-    ret = conn.root.exposed_login(msg,printt)
-    while ret=='Erro':
+	''' Executa login do usuário no servidor. '''
+    
+	msg=input("Entre com o seu nome de usuário, sem espaços: ")
+    
+	#chama a função de login no servidor, passa a função de callback e o nome do usuário
+	ret = conn.root.exposed_login(msg,printt)
+
+    while ret=='Erro': #garante que não haverá cadastro se houver nome repetido. A verificação é no servidor.
         print("Nome já cadastrado\n")
         msg=input("Entre com o seu nome de usuário, sem espaços: ")
         ret = conn.root.exposed_login(msg, printt)
     print("\n"+ret+"\n")
 
 def logout(conn, bgsrv):
+	'''Executar o logout do usuário'''
+
+	#informa servidor que cliente deseja se desconectar. 
 	ret = conn.root.exposed_stop()
-	bgsrv.stop()
-	conn.close()
+	bgsrv.stop() #para thread de background que escutava a chegada de mensagens e executava a função de callback
+	conn.close() #fecha conexão
 
 def listar_comandos(conn):
+	'''Printa os comandos aceitos pelo servidor '''
 	print(conn.root.exposed_listar_comandos())
 
 def listar_usuarios(conn):
+	'''Printa usuários atualmente ativos '''
 	print(conn.root.exposed_listar_user())
 
 def fazRequisicoes(conn, bgsrv):
-	'''Faz requisicoes ao servidor e exibe o resultado.
-	Entrada: conexao estabelecida com o servidor'''
+	'''Faz requisicoes ao servidor e exibe o resultado. '''
 	# le as mensagens do usuario ate ele digitar 'fim'
-	while True: 
+	while True: #checa se são comandos
 		msg = input("Digite uma mensagem ou um comando:")
 		if msg == '@fim': 
 			logout(conn, bgsrv)
@@ -52,24 +62,14 @@ def fazRequisicoes(conn, bgsrv):
 		elif msg == '@listar':
 			listar_usuarios(conn)
 		else:
-			ret = conn.root.exposed_send(msg)	
+			ret = conn.root.exposed_send(msg)	#solicita envio da mensagem ao servidor.
 			if ret:
 				print(ret)
-		# envia a mensagem do usuario para o servidor
-		#ret = conn.root.exposed_echo(msg)
-
-		# imprime a mensagem recebida
-		#print(ret)
-
-	# encerra a conexao
-	# conn.root.exposed_stop()
-	# bgsrv.stop()
-	# conn.close()
 	
 
 def main():
 	conn = iniciaConexao()
-	bgsrv = rpyc.BgServingThread(conn)
+	bgsrv = rpyc.BgServingThread(conn) #backgroundthread: escuta a chegada de mensagens e executava a função de callback quando msg chega
 	login(conn)
 	listar_comandos(conn)
 	#interage com o servidor ate encerrar
--- tarefa 4/srv.py
+++ tarefa 4/srv.py
@@ -1,93 +1,123 @@
-#Ver documentação em: https://rpyc.readthedocs.io/en/latest/
-
-# Servidor de echo usando RPC 
 import rpyc #modulo que oferece suporte a abstracao de RPC
 import time
-#servidor que dispara um processo filho a cada conexao
-from rpyc.utils.server import ForkingServer,ThreadedServer 
+from rpyc.utils.server import ThreadedServer 
 from threading import Thread
 
 # porta de escuta do servidor de echo
 PORTA = 10004
+
+#contém o nome dos usuários conectados
 lista_user = []
+
+#contém as mensagens enviadas para o usuários que ainda não foram entregues. 
 dicionario = {}
 
-# classe que implementa o servico de echo
+# classe que implementa o servico de bate papo
 class Echo(rpyc.Service):
 
     
-	# executa quando uma conexao eh criada
 	def on_connect(self, conn):
+		''' 	Executa quando uma conexao eh criada '''
 		print("Conexao iniciada:")
 
-	def exposed_stop(self):   # this method has to be exposed too
+	def exposed_stop(self):  
+		'''
+		É chamada pelo cliente quando o mesmo deseja encerrar a conexão.
+		'''
+		#Encerra execução da thread que checa constantemente se há mensagens para o cliente.
 		self.active = False
 		self.thread.join()
+		
+		#Remove a lista de mensagens do cliente.
 		dicionario.pop(self.name)
+		
+		#Remove o cliente da liste de conectados.
 		lista_user.remove(self.name)
+		
+		#Adiciona uma mensagem avisando que o cliente saiu na lista de mensagens de todos os outros usuários.
 		for i in range (0, len(lista_user)):
 			dicionario[lista_user[i]].append("\nUsuário " + self.name + " saiu do chat.")
 
-	# executa quando uma conexao eh fechada
+	
 	def on_disconnect(self, conn):
+	# executa quando uma conexao eh fechada
 		print("\nConexao com " + self.name + " finalizada")
 
-	# imprime e ecoa a mensagem recebida
-	def exposed_echo(self, msg):
-		print(msg)
-		return msg
 
 	def exposed_login(self, msg,callback):
-
+		'''
+		Executa o login do cliente.
+		'''
 		self.name=msg
 
+		#checa se nome já foi utilizado
 		if msg in lista_user:
 			print("Usuário " + str(msg) + " já cadastrado.")
 			msg="Erro"
 		else:
-			self.callback = rpyc.async_(callback)   # create an async callback
+			#Guarda na classe a função de callback, que será chamada quando ouver mensagem para cliente. Essa função
+			#será executada no cliente, assíncronamente. No cliente essa função é print.
+			self.callback = rpyc.async_(callback)   
+			
+			#Cria no dicionario uma lista para guardar as mensagens do cliente.
 			dicionario[msg]=list()
+
+			#Cria nova thread para ficar checando se há novas mensagens para o cliente.
 			self.active = True
 			self.thread = Thread(target = self.work)
 			self.thread.start()
+			
+			#Adiciona o cliente na lista de usuários.
 			lista_user.append(msg)
+			
 			msg="Cadastro Realizado com Sucesso"
 			print(lista_user)
 
 		return msg
 
 	def exposed_listar_comandos(self):
-		msg = "Comandos:\nEnviar mensagem(@usuário mensagem)\nListar usuário(@listar)\nListar Possibilidades(@help)\nFechar conexão(close_conn)\n\n"
+		'''
+		Retorna os comandos disponíveis.
+		'''
+		msg = "Comandos:\nEnviar mensagem(@usuário mensagem)\nListar usuário(@listar)\nListar Possibilidades(@help)\nFechar conexão(@fim)\n\n"
 		return msg
 
 	def exposed_listar_user(self):
+		'''
+		Listar os usuários onlines no momento.
+		'''
 		msg = ''
 		for i in lista_user:
 			msg=msg+i+','
 		return msg  
 
 	def exposed_send(self,msg):
-
+		'''
+		Envia a mensagem.
+		'''
 		temp_lista = msg.split()
 		temp = []
 		verifica_usuarios = []
 		tamanho_string_usuario = 0
-
+		
+		#checa se o indicador de comando(@) está presente .
 		if temp_lista[0][0] != '@':
 			return "Comando inválido\nTente digitar @NomeDoUsuario mensagem sem espaços antes do @"
-		else:
+		else:  #checa se há outros destinatários para a mensagem, se houver, adiciona na variável temp
 			for i in range (0, len(temp_lista)):
 				if temp_lista[i][0] == "@":
 					temp.append(temp_lista[i][1:].strip())
 				else:
 					break
-		
+		#se cliente destinatário não está na lista de clientes do servidor então cliente é adicionado na lista verifica_usuarios,
+		#se está na lista de clientes então seu tamanho é contabilizado.
 		for i in range(0, len(temp)):
 			if temp[i] not in lista_user:
 				verifica_usuarios.append(temp[i])
 			else:
 				tamanho_string_usuario += (len(temp[i]) + 2)
 
+		#verifica se usuários na lista verifica_usuarios realmente são clientes ativos.
 		if len(verifica_usuarios) > 0:
 			if len(verifica_usuarios) == 1:
 				msg = "\nUsuário : \n"
@@ -103,8 +133,10 @@ class Echo(rpyc.Service):
 
 			return msg
 
+		#pega parte da mensagem, excluindo nomes do destinatários.
 		msg = msg[tamanho_string_usuario:]
 
+		#salva mensagem no dicionário, com nome de quem enviou na frente
 		for i in range (0, len(temp)):
 			if temp[i] in dicionario.keys():
 				dicionario[temp[i]].append(self.name + " : " + msg)
@@ -120,6 +152,11 @@ class Echo(rpyc.Service):
 
 
 	def work(self):
+		'''
+		Verifica constantemente se há mensagem para o cliente. Se ouver, retorna a última mensagem da lista, e remove
+		a mesma da lista.
+		A função callback será chamada assíncronamente no cliente quando houver mensagem para o cliente.
+		'''
 		while self.active:
 			if len(dicionario[self.name]) > 0:
 				msg = dicionario[self.name][-1]
